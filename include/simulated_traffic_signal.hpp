fe9f4bb66fb02ae11b307fd0d7f0ba209aa4b5c6
Marko Mizdrak <marko.mizdrak@dlr.de>
2025-01-08 11:58:03 +0100

    nicer formatting

--- include/simulated_traffic_signal.hpp
+++ include/simulated_traffic_signal.hpp
@@ -1,34 +1,41 @@
-#include <rclcpp/rclcpp.hpp>
 #include <string>
 #include <vector>
 
 #include "adore_ros2_msgs/msg/traffic_signals.hpp"
+
 #include "std_msgs/msg/header.hpp"
-namespace adore {
+#include <rclcpp/rclcpp.hpp>
+
+namespace adore
+{
 
 using TrafficSignalsMsg = adore_ros2_msgs::msg::TrafficSignals;
-using TrafficSignalMsg = adore_ros2_msgs::msg::TrafficSignal;
+using TrafficSignalMsg  = adore_ros2_msgs::msg::TrafficSignal;
 
-class SimulatedTrafficSignal : public rclcpp::Node {
- public:
+class SimulatedTrafficSignal : public rclcpp::Node
+{
+public:
+
   SimulatedTrafficSignal();
 
- private:
-  struct TrafficLightConfig {
-    double x;
-    double y;
-    double red_duration;
-    double yellow_duration;
-    double green_duration;
-    int32_t state;
+private:
+
+  struct TrafficLightConfig
+  {
+    double       x;
+    double       y;
+    double       red_duration;
+    double       yellow_duration;
+    double       green_duration;
+    int32_t      state;
     rclcpp::Time last_transition_time;
   };
 
   void update_signals();
 
   rclcpp::Publisher<TrafficSignalsMsg>::SharedPtr publisher;
-  rclcpp::TimerBase::SharedPtr timer;
+  rclcpp::TimerBase::SharedPtr                    timer;
 
   std::vector<TrafficLightConfig> traffic_lights;
 };
-}  // namespace adore
+} // namespace adore
--- src/simulated_traffic_signal.cpp
+++ src/simulated_traffic_signal.cpp
@@ -1,68 +1,76 @@
 #include "simulated_traffic_signal.hpp"
 
 using namespace std::chrono_literals;
-namespace adore {
 
-SimulatedTrafficSignal::SimulatedTrafficSignal() : Node("traffic_signal_node") {
+namespace adore
+{
+
+SimulatedTrafficSignal::SimulatedTrafficSignal() :
+  Node( "traffic_signal_node" )
+{
   // Declare parameters
-  declare_parameter("traffic_lights", std::vector<std::string>{});
+  declare_parameter( "traffic_lights", std::vector<std::string>{} );
   std::vector<std::string> traffic_lights_names;
-  get_parameter("traffic_lights", traffic_lights_names);
+  get_parameter( "traffic_lights", traffic_lights_names );
 
-  for (const auto &light_name : traffic_lights_names) {
+  for( const auto &light_name : traffic_lights_names )
+  {
     double x, y, red_duration, yellow_duration, green_duration;
-    declare_parameter(light_name + ".x", 0.0);
-    declare_parameter(light_name + ".y", 0.0);
-    declare_parameter(light_name + ".red_duration", 5.0);
-    declare_parameter(light_name + ".yellow_duration", 2.0);
-    declare_parameter(light_name + ".green_duration", 5.0);
+    declare_parameter( light_name + ".x", 0.0 );
+    declare_parameter( light_name + ".y", 0.0 );
+    declare_parameter( light_name + ".red_duration", 5.0 );
+    declare_parameter( light_name + ".yellow_duration", 2.0 );
+    declare_parameter( light_name + ".green_duration", 5.0 );
 
-    get_parameter(light_name + ".x", x);
-    get_parameter(light_name + ".y", y);
-    get_parameter(light_name + ".red_duration", red_duration);
-    get_parameter(light_name + ".yellow_duration", yellow_duration);
-    get_parameter(light_name + ".green_duration", green_duration);
+    get_parameter( light_name + ".x", x );
+    get_parameter( light_name + ".y", y );
+    get_parameter( light_name + ".red_duration", red_duration );
+    get_parameter( light_name + ".yellow_duration", yellow_duration );
+    get_parameter( light_name + ".green_duration", green_duration );
 
-    traffic_lights.emplace_back(
-        TrafficLightConfig{x, y, red_duration, yellow_duration, green_duration,
-                           TrafficSignalMsg::RED, now()});
+    traffic_lights.emplace_back( TrafficLightConfig{ x, y, red_duration, yellow_duration, green_duration, TrafficSignalMsg::RED, now() } );
   }
 
   // Publisher
-  publisher = create_publisher<TrafficSignalsMsg>("traffic_signals", 10);
+  publisher = create_publisher<TrafficSignalsMsg>( "traffic_signals", 10 );
 
   // Timer
-  timer = create_wall_timer(
-      std::chrono::milliseconds(100),
-      std::bind(&SimulatedTrafficSignal::update_signals, this));
+  timer = create_wall_timer( std::chrono::milliseconds( 100 ), std::bind( &SimulatedTrafficSignal::update_signals, this ) );
 }
 
-void SimulatedTrafficSignal::update_signals() {
+void
+SimulatedTrafficSignal::update_signals()
+{
   TrafficSignalsMsg msg;
   msg.header.stamp = now();
 
-  for (auto &light : traffic_lights) {
+  for( auto &light : traffic_lights )
+  {
     auto current_time = now();
-    auto elapsed_time = (current_time - light.last_transition_time).seconds();
+    auto elapsed_time = ( current_time - light.last_transition_time ).seconds();
 
-    switch (light.state) {
+    switch( light.state )
+    {
       case TrafficSignalMsg::RED:
-        if (elapsed_time >= light.red_duration) {
-          light.state = TrafficSignalMsg::GREEN;
+        if( elapsed_time >= light.red_duration )
+        {
+          light.state                = TrafficSignalMsg::GREEN;
           light.last_transition_time = current_time;
         }
         break;
 
       case TrafficSignalMsg::GREEN:
-        if (elapsed_time >= light.green_duration) {
-          light.state = TrafficSignalMsg::YELLOW;
+        if( elapsed_time >= light.green_duration )
+        {
+          light.state                = TrafficSignalMsg::YELLOW;
           light.last_transition_time = current_time;
         }
         break;
 
       case TrafficSignalMsg::YELLOW:
-        if (elapsed_time >= light.yellow_duration) {
-          light.state = TrafficSignalMsg::RED;
+        if( elapsed_time >= light.yellow_duration )
+        {
+          light.state                = TrafficSignalMsg::RED;
           light.last_transition_time = current_time;
         }
         break;
@@ -73,20 +81,23 @@ void SimulatedTrafficSignal::update_signals() {
     }
 
     TrafficSignalMsg signal_msg;
-    signal_msg.x = light.x;
-    signal_msg.y = light.y;
-    signal_msg.signal_group_id = 1;  // Example ID
-    signal_msg.state = light.state;
+    signal_msg.x               = light.x;
+    signal_msg.y               = light.y;
+    signal_msg.signal_group_id = 1; // Example ID
+    signal_msg.state           = light.state;
 
-    msg.signals.push_back(signal_msg);
+    msg.signals.push_back( signal_msg );
   }
 
-  publisher->publish(msg);
+  publisher->publish( msg );
 }
-}  // namespace adore
-int main(int argc, char **argv) {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<adore::SimulatedTrafficSignal>());
+} // namespace adore
+
+int
+main( int argc, char **argv )
+{
+  rclcpp::init( argc, argv );
+  rclcpp::spin( std::make_shared<adore::SimulatedTrafficSignal>() );
   rclcpp::shutdown();
   return 0;
 }
