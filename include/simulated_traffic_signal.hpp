15f3381c6c8894ebd4e0c88d43724ab86b7cbed7
Marko Mizdrak <marko.mizdrak@dlr.de>
2025-01-08 09:58:35 +0000

    first commit

---
+++ include/simulated_traffic_signal.hpp
@@ -0,0 +1,34 @@
+#include <rclcpp/rclcpp.hpp>
+#include <string>
+#include <vector>
+
+#include "adore_ros2_msgs/msg/traffic_signals.hpp"
+#include "std_msgs/msg/header.hpp"
+namespace adore {
+
+using TrafficSignalsMsg = adore_ros2_msgs::msg::TrafficSignals;
+using TrafficSignalMsg = adore_ros2_msgs::msg::TrafficSignal;
+
+class SimulatedTrafficSignal : public rclcpp::Node {
+ public:
+  SimulatedTrafficSignal();
+
+ private:
+  struct TrafficLightConfig {
+    double x;
+    double y;
+    double red_duration;
+    double yellow_duration;
+    double green_duration;
+    int32_t state;
+    rclcpp::Time last_transition_time;
+  };
+
+  void update_signals();
+
+  rclcpp::Publisher<TrafficSignalsMsg>::SharedPtr publisher;
+  rclcpp::TimerBase::SharedPtr timer;
+
+  std::vector<TrafficLightConfig> traffic_lights;
+};
+}  // namespace adore
